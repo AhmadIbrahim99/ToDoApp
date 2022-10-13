208e6a8b2e463a7e92941a23c3f4c7c6ff2d159a
AhmadIbrahim99 <43531161+AhmadIbrahim99@users.noreply.github.com>
2022-10-13 22:29:49 +0300

    fix build

--- ClassLibrary.Common/Exceptions/UnauthorizedResultException.cs
+++ ClassLibrary.Common/Exceptions/UnauthorizedResultException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebApplication5.Exceptions;
+using ClassLibrary.Common.Exceptions;
 
 namespace ClassLibrary.Common.Exceptions
 {
